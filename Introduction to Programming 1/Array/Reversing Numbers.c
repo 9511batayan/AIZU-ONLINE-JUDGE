511720387a979abe6be502dc4cb8b975185e57a3
Koji Kawabata <14ec043@ms.dendai.ac.jp>
2017-06-09 00:07:29 +0900

    変更

---
+++ Introduction to Programming 1/Array/Reversing Numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define NUM 100
+int main(void)
+{
+	int n;
+	int i, j;
+	int a[NUM];
+
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)scanf("%d", &a[i]);
+
+	for (j = n - 1; j >= 0; j--){
+		printf("%d", a[j]);
+		printf(" ");
+	}
+	printf("\n");
+
+	return 0;
+}
